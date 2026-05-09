ab9f09f5fab656ebc074ea25e13614886d047733
antoniorb1913 <antoniorb1913@gmail.com>
2026-05-09 20:24:59 +0200

    alumnos titulo padding

--- resources/views/acceso-alumnos.blade.php
+++ resources/views/acceso-alumnos.blade.php
@@ -38,7 +38,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <style>
-        body { background-color: #f4f7f6; padding-top: 10%; }
+        body { background-color:#f4f7f6;min-height:50vh;display:flex;flex-direction:column;padding-top: 10%; }
 
         .navbar-custom { background-color: #ffffff; border-bottom: 2px solid #dee2e6; }
         .student-card { 
