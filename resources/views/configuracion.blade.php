2f26d7bb3c7a80d09010e6c00b542c92b31d7b9f
antoniorb1913 <antoniorb1913@gmail.com>
2026-05-16 17:36:25 +0200

    padding top configuracion

--- resources/views/configuracion.blade.php
+++ resources/views/configuracion.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <style>
-        body { background-color: rgb(244, 242, 238); min-height: 100vh; display: flex; flex-direction: column; }
+        body { background-color: rgb(244, 242, 238); min-height: 100vh; display: flex; flex-direction: column; padding-top: 2%;}
         @media (max-width: 576px) {
         body {
             padding-top: 10%;
