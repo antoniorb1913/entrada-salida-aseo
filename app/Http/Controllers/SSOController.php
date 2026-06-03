79ba9e4b944176c3298d01647c98df0e939300d5
antoniorb1913 <antoniorb1913@gmail.com>
2026-06-03 22:29:49 +0200

    prueba rol

--- app/Http/Controllers/SSOController.php
+++ app/Http/Controllers/SSOController.php
@@ -50,7 +50,7 @@ class SSOController extends Controller
                 $rolRecibido = 'admin';
             }
 
-            if ($rolRecibido === 'profesor') {
+            if ($rolRecibido === 'admin') {
                 $rolRecibido = 'profesor';
             }
 
