c0219639dd00b29671e43310e38222a1119ccd9a
antoniorb1913 <antoniorb1913@gmail.com>
2026-06-06 23:48:05 +0200

    leyenda

--- resources/views/Footer/footer.blade.php
+++ resources/views/Footer/footer.blade.php
@@ -9,6 +9,15 @@
             </span>
         </div>
 
+                {{-- LEYENDA (Mismo tamaño exacto que el de arriba) --}}
+        <div class="d-flex align-items-center justify-content-center bg-white px-4 py-2 rounded-pill shadow-sm border border-warning-subtle" 
+             style="width: 100%; max-width: 850px;">
+            <i class="bi bi-person-heart-add text-warning fs-5 me-2"></i>
+            <span class="text-muted" style="font-size: 0.95rem;">
+                Este símbolo indica que el alumno <strong>tiene una excepción médica</strong> para poder salir mas veces del limite de salidas.
+            </span>
+        </div>
+
         {{-- LEYENDA (Mismo tamaño exacto que el de arriba) --}}
         <div class="d-flex align-items-center justify-content-center bg-white px-4 py-2 rounded-pill shadow-sm border border-warning-subtle" 
              style="width: 100%; max-width: 850px;">
@@ -17,5 +26,7 @@
                 Este símbolo indica que el alumno <strong>requiere un acompañante obligatorio</strong> para poder salir del aula.
             </span>
         </div>
+
     </div>
+    
 </footer>
